8de94ccfb589683b6dd2f0cb957bf18aa8ca4b92
ivankorshunov <ivan.korshunov.1997@gmail.com>
2023-03-21 05:18:27 +0200

    Registration and Log in

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -4,4 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
+
+# Figure admin titles
+admin.site.site_header = 'My Club administration page'
+admin.site.site_title = 'Browser title'
+admin.site.index_title = ''
